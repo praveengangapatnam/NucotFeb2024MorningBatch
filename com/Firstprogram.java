6ba3f4bb892dfda8e36d55de95eefd279d7986fd
praveengangapatnam <160814208+praveengangapatnam@users.noreply.github.com>
2024-03-01 20:50:14 +0530

    Add files via upload

---
+++ com/Firstprogram.java
@@ -0,0 +1,13 @@
+package javasessions.com;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO EVERYONE");
+		System.out.println("GOOD MORNING");
+		System.out. println("WELCOME TO JAVA");
+		System.out.println("INSTALLATION");
+
+	}
+
+}
