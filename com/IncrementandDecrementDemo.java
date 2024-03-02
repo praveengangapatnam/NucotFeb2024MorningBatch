c3de2c5cca4cb26b37408b8b09485f41af63d151
praveengangapatnam <160814208+praveengangapatnam@users.noreply.github.com>
2024-03-02 22:06:19 +0530

    Add files via upload

---
+++ com/IncrementandDecrementDemo.java
@@ -0,0 +1,89 @@
+package javasessions.com;
+
+public class IncrementandDecrementDemo {
+
+	public static void main(String[] args) {
+	
+		//++ and --
+		//post increment -->a++
+		//pre increment -->++a
+		
+		// post decrement -->a--
+		//pre decrement-->--a
+	   
+		System.out.println("post increment");
+		
+		
+         int a=5;
+         int b=a++;
+       
+       System.out.println(a);
+       System.out.println(b);
+       
+         int aa=7;
+         int bb=a++;
+      
+      System.out.println(b); 
+      System.out.println(bb);
+         
+        System.out.println("pre increment");
+        
+        int i=12;
+        int ii=++i;
+        
+      System.out.println(i);
+      System.out.println(ii);
+      
+        int n=14;
+        int nn=++n;
+      System.out.println(n);
+      System.out.println(nn);
+      
+      System.out.println("post drecement");
+      
+        int t=8;
+        int tt=t--;
+      
+      System.out.println(t);
+      System.out.println(tt);
+        
+        int p=6;
+        int pp=p--;
+        
+      System.out.println(p);
+      System.out.println(pp);
+        
+      System.out.println("pre decrement");
+       
+        int c=16;
+        int cc=--c;
+        
+      System.out.println(c);
+      System.out.println(cc);
+        
+        int e=18;
+        int ee=--e;
+      System.out.println(e);
+      System.out.println(ee);
+      
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+	}
+
+}
