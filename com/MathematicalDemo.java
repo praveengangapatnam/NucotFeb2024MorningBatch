c9e9bfde37b35e7edd48f5beeb3c2e74a81787e9
praveengangapatnam <160814208+praveengangapatnam@users.noreply.github.com>
2024-03-02 12:11:17 +0530

    Add files via upload

---
+++ com/MathematicalDemo.java
@@ -0,0 +1,88 @@
+package javasesssions.com;
+
+public class MathematicalDemo {
+
+	public static void main(String[] args) {
+		
+         int a=10;
+         int b=30;
+         
+         System.out.println	(a+b);
+         System.out.println(a-b);
+         System.out.println(a*b);
+         System.out.println(a=b);
+         System.out.println(a%b);
+         System.out.println(a%b);
+         
+         System.out.println("*************************************");
+         
+         String s= "welcome nucot";
+         String ss= "Bangalore";
+         
+         System.out.println(s);
+         System.out.println(ss);
+         System.out.println(s+ss);
+         System.out.println(a+s);
+         System.out.println(a+b+s);
+         System.out.println(a+b+s+ss);
+         System.out.println(s+ss+a+b);
+         System.out.println(s+ss+(a+b));
+         
+         System.out.println("***********************************");
+         
+        char c='a';
+        char cc='b';
+        
+        System.out.println(c+s);
+        System.out.println(cc+s);
+        System.out.println(c+cc+s+ss);
+        System.out.println(s+ss+c+cc);
+        System.out.println(s+ss+(c+cc)); 
+        
+        System.out.println("*********************************");
+        
+        Short sh=12;
+        Short shh=10;
+        
+        System.out.println(s+ss);
+        System.out.println(sh+s);
+        System.out.println(sh+shh+s);
+        System.out.println(sh+shh+s+ss);
+        System.out.println(s+ss+sh+shh);
+        System.out.println(s+ss+(sh+shh));
+        
+        System.out.println("***********************************");
+        
+        float f=30.0f;
+        float ff=50.0f;
+        
+        System.out.println(f+ff);
+        System.out.println(f+s);
+        System.out.println(f+s+sh);
+        System.out.println(f+ff+a+b);
+        
+        System.out.println("*************************************");
+        
+        double d=4.5;
+        double dd=5.5;
+        
+        System.out.println(d+s);
+        System.out.println(d+dd+s);
+        System.out.println(s+d+dd);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+
+}
+
