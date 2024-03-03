7cc53cca63f548f6100eae637cd4b5a264c1a59d
praveengangapatnam <160814208+praveengangapatnam@users.noreply.github.com>
2024-03-03 14:11:24 +0530

    Add files via upload

---
+++ com/Firstkeyword.java
@@ -0,0 +1,54 @@
+package javasessions.com;
+
+public class Firstkeyword {
+
+	public static void main(String[] args) {
+		
+				 
+				
+				int a=5;
+				        
+		                 a=6;
+		                 a=10;
+		                
+		                System.out.println(a);
+		                
+		         final int days=7;
+		         
+		                 System.out.println(days);
+		                
+		         final float pi=3.14f;
+		                  
+		                  System.out.println(pi);
+		                   
+		                
+		         final String companyname="Nucot";
+		                  
+		                  System.out.println(companyname);
+		                  
+		          final char gendermale='m';
+		        		 
+		        		  System.out.println(gendermale);
+		          
+		          final char genderfemale='f';
+		          
+		                  System.out.println(genderfemale);
+		                  
+		                
+		                
+		                
+		                
+		                
+			}
+			
+			
+			
+			
+			
+			
+
+		
+
+	}
+
+
